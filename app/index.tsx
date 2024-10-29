fe2f6ade1003b732a0ea981d4b7a913eed7726e8
Harlem Kirua <debabel1hackamani@gmail.com>
2024-10-29 02:23:33 +0000

    home screen first commit

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+const index = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+});
+export default index;
